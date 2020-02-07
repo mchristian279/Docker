cda08da37ff2f34f04fac622c7721047ea3c4235
Administrator <admin@example.com>
2020-02-07 10:31:14 -0300

    Build e push Imagem Registry

---
+++ Build-Push-Imagem.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Autor: Michael Reis
+
+# Variaveis Externas do Jenkins
+#${ImagemBase}
+#${Tag}
+
+# Variaveis de Ambiente Globais
+GitRepo="ProjetoGit"
+GitRepoDir=`echo "$GitRepo" | cut -d '/' -f 5 | cut -d '.' -f 1`
+UserRegistry="USUARIOREGISTRY"
+PassRegistry="SENHA"
+UrlRegistry="URLREGISTRY:18444"
+
+# Login Registry - Loga na Registry privada para Pull e Push de Imagens Base
+Login_Registry () {
+    echo "Logando Registry" ;
+    docker login -u "$UserRegistry" -p "$PassRegistry" "$UrlRegistry" 
+}
+
+# Build Imagem Docker - Recebe como parâmetro as variáveis ${ImagemBase} e ${Tag} do Jenkins
+Build_Imagem () {
+    echo "Realizando Build da Imagem..." ;
+    docker build -t "$UrlRegistry"/"${ImagemBase}":"${Tag}" "$GitRepoDir"/
+}
+
+# Push Imagem Docker - Envia a Imagem Gerada para a Registry Privada
+Push_Imagem () {
+    echo "Versionando Imagem na Registry..." ;
+    docker push "$UrlRegistry"/"${ImagemBase}":"${Tag}"  
+}
+
+# Executa a Função Login_Registry
+Login_Registry ; 
+
+# Executa a Função Build_Imagem
+Build_Imagem
+
+# Valida a execução da chamada da Função Build_Imagem
+if [[ $? == 0 ]]; then
+    Push_Imagem ;
+    sleep 2;
+    echo "Push "${ImagemBase}":"${Tag}" Realizado com Sucesso! "
+else
+    echo "Não foi possível realizar o Build da "${ImagemBase}":"${Tag}" " ;
+exit
+fi
